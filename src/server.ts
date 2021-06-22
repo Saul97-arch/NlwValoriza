e0831cb9573f2a0f4de6914b5c251d395c3c0adb
saulo <saulo@pop-os.localdomain>
2021-06-22 20:28:33 -0300

    Exception treatment done via middleware in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 
-import express from "express";
+import "express-async-errors";
+
+import express, { Request, Response, NextFunction } from "express";
 
 import { router } from "./routes";
 
@@ -11,6 +13,22 @@ const app = express();
 app.use(express.json())
 
 app.use(router);
+
+// Tratando erros com middleware
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json(
+            {
+                error: err.message
+            }
+        )
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "internal server error"
+    })
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
