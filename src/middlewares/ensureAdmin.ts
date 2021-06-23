9e7264044223481dca951d31814c560a6b512665
saulo <saulo@pop-os.localdomain>
2021-06-23 06:27:25 -0300

    day 3 done!

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+
+export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    // Verificar se usuário é admin
+
+    const admin = false;
+
+    if (admin) {
+        console.log("Entrou no if");
+        return next();
+    }
+    console.log("Não entrou");
+    return response.status(401).json({
+        error: "Unauthorized",
+    });
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
-
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
-router.post("/users", createUserController.handle); 
+// ensureAdmin passado como middleware na rota, pode ir adicionando o quanto quiser antes da requisição
+router.post("/users",createUserController.handle); 
+router.post("/tags", ensureAdmin ,createTagController.handle)
 
 export { router };
